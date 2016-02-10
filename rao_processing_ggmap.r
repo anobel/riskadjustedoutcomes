aa8d351f9611e54e0ad1fbadeaddcf56792f579b
anobel <anospamo@gmail.com>
2016-02-10 09:26:56 -0800

    calculate driving times/distances for urologic cohort

---
+++ rao_processing_ggmap.r
@@ -0,0 +1,45 @@
+# load packages
+library(ggmap)
+library(dplyr)
+
+# Change this to relevant working directory
+setwd("~/Documents/code/rao")
+
+# load the dataframe of addresses for which we want to calculate driving distances
+# For now, because google maps API limit is 2500 requests per day, will only do the urology cohort
+# which is about 20k combinations of to/from zip codes
+ptgu <- readRDS("rao_workingdata/ptgu.rds")
+
+# make dataframe of unique zip combinations and drop obs with mizzing zips
+ptzips <- ptgu %>%
+  filter(!is.na(patzcta) & !is.na(hospzcta)) %>%
+  select(
+    patzcta, hospzcta
+  )%>%
+  mutate(
+    patzcta = as.character(patzcta),
+    hospzcta = as.character(hospzcta)) %>%
+  unique()
+
+# calculate distances using mapdist() in ggmap package
+# but due to google API limit of 2500 reuests per day, have to do it in a series of chunks and combine them
+driving <- lapply(1:2500, function (x) mapdist(as.character(ptzips[x,"patzcta"]), as.character(ptzips[x,"hospzcta"]), mode="driving", output="simple"))
+
+# drop any zip code pairs with missing/incomplete data
+driving <- driving[sapply(driving,length)==8]
+
+# make an empty data frame the first time this is done, and then bind to the prior data
+# drivingdf <- list()
+drivingdf <- rbind(drivingdf,do.call(rbind, driving))
+
+driving <- driving %>%
+  select(
+    patzcta = from,
+    hospzcta = to,
+    km, miles,
+    hours
+  )
+
+head(driving)
+
+saveRDS(driving, file="rao_workingdata/distances.rds")
