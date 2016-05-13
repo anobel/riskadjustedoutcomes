f2023e19bdacf542c6ad5ab31ae2993b2bc82c67
anobel <anospamo@gmail.com>
2016-05-13 10:57:02 -0700

    convert Census ACS data to csv update processing code make csv available

--- rao_processing_acs.r
+++ rao_processing_acs.r
@@ -1,40 +1,79 @@
+# Load neccesary libraries
 library(stringr)
 library(acs)
 library(dplyr)
 
-# to use the ACS package to directly download ACS data, need to sign up for API key and install it (once)
-#api.key.install("d5481ffa6d9e8cb41a08e565fbd4d6baa46cf350")
-zcta <- readRDS("rao_workingdata/zcta.rds")
+# To use the ACS package to directly download ACS data, need to sign up for API key and install it (once)
 
-# define the geographic unit (ZCTAs) for data
-# this cannot be combined with any other options, so get all ZCTAs then drop non-CA
+# api.key <- "d5481ffa6d9e8cb41a08e565fbd4d6baa46cf350"  # ENTER YOUR OWN API KEY HERE
+# api.key.install(api.key)
+
+# Import ZIP/ZCTA crosswalk (tidy)
+# This can be generated from raw data in rao_processing_zip.r
+zcta <- read.csv("data/tidy/zcta.csv")
+
+# Define the geographic unit (ZCTAs) for data
+# This cannot be combined with any other options, so get all ZCTAs then drop non-CA
 geo_zip = geo.make(zip.code = "*")
 
 # Download the data for desired tables directly from the American Community Survey
 acsd <- NULL
+
 # B01003 Total Population
-acsd$pop <- acs.fetch(geo_zip, table.number = "B01003", endyear=2011, span=5, col.names = "pretty")
+acsd$pop <- acs.fetch(geo_zip,
+                      table.number = "B01003",
+                      endyear = 2011,
+                      span = 5,
+                      col.names = "pretty")
+
 # Keep only California ZCTAs
 acsd$pop <- acsd$pop[geography(acsd$pop)$zipcodetabulationarea %in% zcta$zcta]
 
 # B03002 - Ethnicity Data
-acsd$ethnicity <- acs.fetch(geo_zip, table.number = "B03002", endyear=2011, span=5, col.names = "pretty")
+acsd$ethnicity <- acs.fetch(geo_zip,
+                            table.number = "B03002",
+                            endyear = 2011,
+                            span = 5,
+                            col.names = "pretty")
+
+# Keep only California ZCTAs
 acsd$ethnicity <- acsd$ethnicity[geography(acsd$ethnicity)$zipcodetabulationarea %in% zcta$zcta]
 
-acsd$ethwhite <- divide.acs(num=acsd$ethnicity[,3], den=acsd$ethnicity[,1], method="proportion")
-acsd$ethblack <- divide.acs(num=acsd$ethnicity[,4], den=acsd$ethnicity[,1], method="proportion")
-acsd$ethlatin <- divide.acs(num=acsd$ethnicity[,12], den=acsd$ethnicity[,1], method="proportion")
-acsd$ethasian <- divide.acs(num=acsd$ethnicity[,6], den=acsd$ethnicity[,1], method="proportion")
-acsd$ethother <- apply(X=acsd$ethnicity[,c(5,7:11)], 2, FUN=sum, agg.term="ethother") 
-acsd$ethother <- divide.acs(num=acsd$ethother, den=acsd$ethnicity[,1], method="proportion")
+# Calculate summary variables for each ethnicity
+acsd$ethwhite <- divide.acs(num = acsd$ethnicity[, 3],
+                              den = acsd$ethnicity[, 1],
+                              method = "proportion")
+acsd$ethblack <- divide.acs(num = acsd$ethnicity[, 4],
+                              den = acsd$ethnicity[, 1],
+                              method = "proportion")
+acsd$ethlatin <- divide.acs(num = acsd$ethnicity[, 12],
+                              den = acsd$ethnicity[, 1],
+                              method = "proportion")
+acsd$ethasian <- divide.acs(num = acsd$ethnicity[, 6],
+                              den = acsd$ethnicity[, 1],
+                              method = "proportion")
+acsd$ethother <- apply(X = acsd$ethnicity[, c(5, 7:11)], 2, 
+                      FUN = sum, agg.term = "ethother")
+acsd$ethother <- divide.acs(num = acsd$ethother,
+                              den = acsd$ethnicity[, 1],
+                              method = "proportion")
 
 # B19013 - Median Household Income
-acsd$medianincome <- acs.fetch(geo_zip, table.number = "B19013", endyear=2011, span=5, col.names = "pretty")
+acsd$medianincome <- acs.fetch(geo_zip,
+                                table.number = "B19013",
+                                endyear = 2011,
+                                span=5,
+                                col.names = "pretty")
 # Keep only California ZCTAs
 acsd$medianincome <- acsd$medianincome[geography(acsd$medianincome)$zipcodetabulationarea %in% zcta$zcta]
 
 # B19001 - Household Income Groupings
-acsd$incomegroups <- acs.fetch(geo_zip, table.number = "B19001", endyear=2011, span=5, col.names = "pretty")
+acsd$incomegroups <- acs.fetch(geo_zip,
+                              table.number = "B19001",
+                              endyear = 2011,
+                              span = 5,
+                              col.names = "pretty")
+
 # Keep only California ZCTAs
 acsd$incomegroups <- acsd$incomegroups[geography(acsd$incomegroups)$zipcodetabulationarea %in% zcta$zcta]
 
@@ -43,34 +82,58 @@ acsd$incomegroups <- acsd$incomegroups[geography(acsd$incomegroups)$zipcodetabul
 # Household Income    $10,000 to $14,999
 # Household Income    $15,000 to $19,999
 # Household Income    $20,000 to $24,999
-acsd$incomeunder25k <- apply(X=acsd$incomegroups[,c(2:5)], 2, FUN=sum, agg.term="incomeunder25k") 
-acsd$incomeunder25k <- divide.acs(num=acsd$incomeunder25k, den=acsd$incomegroups[,1], method="proportion")
+acsd$incomeunder25k <- apply(X = acsd$incomegroups[, c(2:5)], 2,
+                            FUN = sum, agg.term = "incomeunder25k")
+acsd$incomeunder25k <- divide.acs(num = acsd$incomeunder25k,
+                                  den = acsd$incomegroups[, 1],
+                                  method = "proportion")
 
 # for income over 100k, combine these groups then divide by total to create proportion
 # Household Income  $100,000 to $124,999
 # Household Income  $125,000 to $149,999
 # Household Income  $150,000 to $199,999
 # Household Income      $200,000 or more
-acsd$incomeover100k <- apply(X=acsd$incomegroups[,c(14:17)], 2, FUN=sum, agg.term="incomeover100k") 
-acsd$incomeover100k <- divide.acs(num=acsd$incomeover100k, den=acsd$incomegroups[,1], method="proportion")
+acsd$incomeover100k <- apply(X = acsd$incomegroups[, c(14:17)], 2,
+                            FUN = sum, agg.term = "incomeover100k")
+acsd$incomeover100k <- divide.acs(num = acsd$incomeover100k,
+                                  den = acsd$incomegroups[, 1],
+                                  method = "proportion")
 
 # B19054 - Interest, Dividends, or Net Rental Income for Households 
-acsd$capincome <- acs.fetch(geo_zip, table.number = "B19054", endyear=2011, span=5, col.names = "pretty")
+acsd$capincome <- acs.fetch(geo_zip,
+                            table.number = "B19054",
+                            endyear = 2011,
+                            span = 5,
+                            col.names = "pretty")
+
 # Keep only California ZCTAs
 acsd$capincome <- acsd$capincome[geography(acsd$capincome)$zipcodetabulationarea %in% zcta$zcta]
 
 # Calculate the proportion of households that have interest, dividend, or rental income
-# used divide method in ACS package to also generate appropriate standard errors
-acsd$capincome <- divide.acs(num=acsd$capincome[,2], den=acsd$capincome[,1], method="proportion")
+# Used divide method in ACS package to also generate appropriate standard errors
+acsd$capincome <- divide.acs(num = acsd$capincome[, 2],
+                             den = acsd$capincome[, 1],
+                             method = "proportion")
 
 # B25077 - Median Value (Dollars) for Owner-Occupied Housing Units
-acsd$housevalue <- acs.fetch(geo_zip, table.number = "B25077", endyear=2011, span=5, col.names = "pretty")
+acsd$housevalue <- acs.fetch(geo_zip,
+                             table.number = "B25077",
+                             endyear = 2011,
+                             span = 5,
+                             col.names = "pretty")
 # Keep only California ZCTAs
 acsd$housevalue <- acsd$housevalue[geography(acsd$housevalue)$zipcodetabulationarea %in% zcta$zcta]
 
 # B15002  Sex by Educational Attainment for the Population 25 Years and over
-acsd$edu <- acs.fetch(geo_zip, table.number="B15002", endyear=2011, span=5, col.names = "pretty")
+acsd$edu <- acs.fetch(geo_zip,
+                      table.number = "B15002",
+                      endyear = 2011,
+                      span = 5,
+                      col.names = "pretty")
+
+# Keep only California ZCTAs
 acsd$edu <- acsd$edu[geography(acsd$edu)$zipcodetabulationarea %in% zcta$zcta]
+
 # Need to combine columns to get the total number of people (males and females) that did not complete HS
 # combined the following columns
 # Sex by Educational Attainment for the Population 25 Years and over: No schooling completed 
@@ -81,9 +144,13 @@ acsd$edu <- acsd$edu[geography(acsd$edu)$zipcodetabulationarea %in% zcta$zcta]
 # Sex by Educational Attainment for the Population 25 Years and over: 10th grade 
 # Sex by Educational Attainment for the Population 25 Years and over: 11th grade 
 # Sex by Educational Attainment for the Population 25 Years and over: 12th grade, no diploma 
-acsd$edunohs <- apply(X=acsd$edu[,c(3:10, 20:27)], 2, FUN=sum, agg.term="nohs") 
+acsd$edunohs <- apply(X = acsd$edu[, c(3:10, 20:27)], 2,
+                      FUN = sum, agg.term = "nohs")
+
 # divide by total ZCTA population (using ACS functions) to get proportion
-acsd$edunohs <- divide.acs(num=acsd$edunohs, den=acsd$edu[,1], method="proportion")
+acsd$edunohs <- divide.acs(num = acsd$edunohs,
+                           den = acsd$edu[, 1],
+                           method = "proportion")
 
 # need to combine columns to get the total number of people (males and females) that have completed HS
 # combined these columns:
@@ -96,74 +163,103 @@ acsd$edunohs <- divide.acs(num=acsd$edunohs, den=acsd$edu[,1], method="proportio
 # Sex by Educational Attainment for the Population 25 Years and over: Professional school degree
 # Sex by Educational Attainment for the Population 25 Years and over: Doctorate degree
 
-acsd$eduhs <- apply(X=acsd$edu[,c(11:18,28:35)], 2, FUN=sum, agg.term="hs") 
+acsd$eduhs <- apply(X = acsd$edu[, c(11:18, 28:35)], 2,
+                    FUN = sum, agg.term = "hs")
+
 # divide by total ZCTA population (using ACS functions) to get proportion
-acsd$eduhs <- divide.acs(num=acsd$eduhs, den=acsd$edu[,1], method="proportion")
+acsd$eduhs <- divide.acs(num = acsd$eduhs,
+                         den=acsd$edu[, 1],
+                         method = "proportion")
 
 # For numbers that completed college, combined the last 4 columns above
-acsd$educollege <- apply(X=acsd$edu[,c(15:18,32:35)], 2, FUN=sum, agg.term="college") 
+acsd$educollege <- apply(X = acsd$edu[, c(15:18, 32:35)], 2,
+                         FUN = sum, agg.term = "college")
+
 # divide by total ZCTA population (using ACS functions)
-acsd$educollege <- divide.acs(num=acsd$educollege, den=acsd$edu[,1], method="proportion")
+acsd$educollege <- divide.acs(num = acsd$educollege,
+                              den = acsd$edu[, 1],
+                              method = "proportion")
 
 # C24010 Sex by Occupation for the Civilian Employed Population 16 years and over
-acsd$emp <- acs.fetch(geo_zip, table.number="C24010", endyear=2011, span=5, col.names = "pretty")
+acsd$emp <- acs.fetch(geo_zip,
+                      table.number = "C24010",
+                      endyear = 2011,
+                      span = 5,
+                      col.names = "pretty")
+
 # Keep only California ZCTAs
 acsd$emp <- acsd$emp[geography(acsd$emp)$zipcodetabulationarea %in% zcta$zcta]
 
 # Add up columns for "Management, business, science, and arts occupations", male and female
-acsd$empexec <- apply(X=acsd$emp[,c(3, 39)], 2, FUN=sum, agg.term = "exec")
-acsd$empexec <- divide.acs(num=acsd$empexec, den=acsd$pop, method="proportion")
+acsd$empexec <- apply(X = acsd$emp[, c(3, 39)], 2,
+                      FUN = sum, agg.term = "exec")
+acsd$empexec <- divide.acs(num = acsd$empexec,
+                           den = acsd$pop,
+                           method="proportion")
 
 # B19083 Gini Index Of Income Inequality
-acsd$gini <- acs.fetch(geo_zip, table.number="B19083", endyear=2011, span=5, col.names = "pretty")
+acsd$gini <- acs.fetch(geo_zip,
+                       table.number = "B19083",
+                       endyear = 2011,
+                       span = 5,
+                       col.names = "pretty")
+
 # Keep only California ZCTAs
 acsd$gini <- acsd$gini[geography(acsd$gini)$zipcodetabulationarea %in% zcta$zcta]
 
 acs <- data.frame(
   zcta = as.numeric(geography(acsd$pop)$zipcodetabulationarea),
   pop = as.numeric(estimate(acsd$pop)),
-  ethwhite = as.numeric(estimate(acsd$ethwhite))*100,
-  ethblack = as.numeric(estimate(acsd$ethblack))*100,
-  ethlatin = as.numeric(estimate(acsd$ethlatin))*100,
-  ethasian = as.numeric(estimate(acsd$ethasian))*100,
-  ethother = as.numeric(estimate(acsd$ethother))*100,
+  ethwhite = as.numeric(estimate(acsd$ethwhite)) * 100,
+  ethblack = as.numeric(estimate(acsd$ethblack)) * 100,
+  ethlatin = as.numeric(estimate(acsd$ethlatin)) * 100,
+  ethasian = as.numeric(estimate(acsd$ethasian)) * 100,
+  ethother = as.numeric(estimate(acsd$ethother)) * 100,
   medianincome = as.numeric(estimate(acsd$medianincome)),
   medianincomelog = log(as.numeric(estimate(acsd$medianincome))),
-  incomeunder25k = as.numeric(estimate(acsd$incomeunder25k))*100,
-  incomeover100k = as.numeric(estimate(acsd$incomeover100k))*100,
-  capincome = as.numeric(estimate(acsd$capincome))*100,
+  incomeunder25k = as.numeric(estimate(acsd$incomeunder25k)) * 100,
+  incomeover100k = as.numeric(estimate(acsd$incomeover100k)) * 100,
+  capincome = as.numeric(estimate(acsd$capincome)) * 100,
   housevalue = as.numeric(estimate(acsd$housevalue)),
   housevaluelog = log(as.numeric(estimate(acsd$housevalue))),
-  edunohs = as.numeric(estimate(acsd$edunohs))*100,
-  eduhs = as.numeric(estimate(acsd$eduhs))*100,
-  educollege = as.numeric(estimate(acsd$educollege))*100,
-  empexec = as.numeric(estimate(acsd$empexec))*100,
+  edunohs = as.numeric(estimate(acsd$edunohs)) * 100,
+  eduhs = as.numeric(estimate(acsd$eduhs)) * 100,
+  educollege = as.numeric(estimate(acsd$educollege)) * 100,
+  empexec = as.numeric(estimate(acsd$empexec)) * 100,
   gini = as.numeric(estimate(acsd$gini))
   )
 
-# Clean up
+# Clean up environment
 rm(zcta, geo_zip)
 
 # all the NaN were generated by me dividing by zero for population
 # because these were supposed to proportions of population, if 0 population, these are by definition 0
 # replace NaN with 0, but is.nan only works on matrices, not dataframes, so a simple function to fix that
 is.nan.data.frame <- function(x)
-  do.call(cbind, lapply(x, is.nan))
+    do.call(cbind, lapply(x, is.nan))
 acs[is.nan(acs)] <- 0
 
-acs$iceinc <- (acs$incomeover100k - acs$incomeunder25k)/100
-acs$iceedu <- (acs$educollege - acs$edunohs)/100
-acs$iceeth <- (acs$ethwhite - acs$ethblack)/100
+acs$iceinc <- (acs$incomeover100k - acs$incomeunder25k) / 100
+acs$iceedu <- (acs$educollege - acs$edunohs) / 100
+acs$iceeth <- (acs$ethwhite - acs$ethblack) / 100
 
 # clean up environment
 rm(is.nan.data.frame, acsd)
 
 # convert the dataframe from values into z-scores
-acsz<- as.data.frame(cbind(zcta=acs[,1],scale(acs[,c(2:length(acs))], center=T, scale=T)))
+acsz <- as.data.frame(cbind(zcta = acs[, 1],
+                           scale(acs[, c(2:length(acs))], center = T, scale = T)
+                          )
+)
 
 # Calculate combined neighbordhood Z-score
 # Used rowSums after binding columns of interest into a matrix so I could ignore NA's
-acs$hoodzscore <- rowSums(cbind(acsz$medianincomelog, acsz$housevalue, acsz$capincome, acsz$eduhs, acsz$educollege, acsz$empexec), na.rm=T)
+acs$hoodzscore <- rowSums(cbind(acsz$medianincomelog,
+                                acsz$housevalue,
+                                acsz$capincome,
+                                acsz$eduhs,
+                                acsz$educollege,
+                                acsz$empexec), na.rm = T)
 
 # Assign quintiles
 acs$hoodquint <- factor(ntile(acs$hoodzscore, 5))
@@ -171,24 +267,22 @@ acs$hooddecile <- factor(ntile(acs$hoodzscore, 10))
 
 # Create centered and scaled variables, useful for regression interpretation
 # Zip code population for every 10,000 people
-acs$pop10k <- acs$pop/10000
+acs$pop10k <- acs$pop / 10000
 
 # Median household income, for every $1,000
-acs$medianincome1k <- acs$medianincome/1000
-# For now, will not center median income (description of interpretation will be confusing
-# "For each $1k increase in median income over the mean)
-# acs$medianincome1kcenter <- scale(acs$medianincome, center=T, scale=F)/1000
+acs$medianincome1k <- acs$medianincome / 1000
 
 # Median Home Value, foe every $100,000
-acs$housevalue100k <- acs$housevalue/100000
+acs$housevalue100k <- acs$housevalue / 100000
 
 acs <- acs %>%
-  select(zcta, pop, pop10k, 
-         ethwhite, ethblack, ethlatin, ethasian, ethother, 
-         medianincome, medianincome1k, incomeunder25k, incomeover100k, capincome, 
+  select(zcta, pop, pop10k,
+         ethwhite, ethblack, ethlatin, ethasian, ethother,
+         medianincome, medianincome1k, incomeunder25k, incomeover100k,
+         capincome,
          housevalue, housevalue100k,
          edunohs, eduhs, educollege, empexec,
          gini, iceinc, iceedu, iceeth, hoodzscore, hoodquint, hooddecile)
 
-# Save into RDS
-saveRDS(acs, file="rao_workingdata/acs.rds")
+# Save into CSV
+write.csv(acs, file="data/tidy/acs.csv", row.names=F)
