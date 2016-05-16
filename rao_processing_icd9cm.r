0a13eb1f17bff95165aa4bea9f42701862d08cd0
anobel <anospamo@gmail.com>
2016-05-16 09:03:33 -0700

    add Medicare ICD9 labels, raw and tidy convert from RDA to CSV

--- rao_processing_icd9cm.r
+++ rao_processing_icd9cm.r
@@ -4,6 +4,7 @@
 # import and save, keep for future use
 
 # Import procedure codes for 2011, use them as field labels
-icd9cm <- read.fwf(file="rao_originaldata/medicare_icd9cm/v29.y2011.icd9cm.txt", header=F, widths=c(4,1000), stringsAsFactors=F)
-save(icd9cm, file="rao_workingdata/icd9cm.rda")
+# Files originally available from: http://www.cdc.gov/nchs/icd/icd9cm.htm
+icd9cm <- read.fwf(file = "data/raw/medicare_icd9cm/v29.y2011.icd9cm.txt", header = F, widths = c(4, 1000), stringsAsFactors = F)
+write.csv(icd9cm, file="data/tidy/icd9cm.csv", row.names = F)
 rm(icd9cm)
